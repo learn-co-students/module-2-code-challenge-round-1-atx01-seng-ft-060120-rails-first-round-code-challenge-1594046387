adaff452d8a7945aaab7a4c0a3de7e74abaf7627
VenkatNan <nanditavenkat@gmail.com>
2020-07-06 13:12:49 -0500

    submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,24 @@
 class HeroinesController < ApplicationController
+  
   def index
     @heroines = Heroine.all
   end
+  def show
+    @heroines = Heroine.find(params[:id])
+  end
+  
+  def new
+    @heroines = Heroine.new
+  end
+
+  def create()
+      @heroines = Heroine.new(params[:id])
+      @heroines.save
+      redirect_to @heroines
+  end
+
+  # def hero_params
+  #   params.require(:heroine).permit(:name,:super_name)
+  # end
+
 end
---
+++ app/models/heroine_power.rb
@@ -0,0 +1,4 @@
+class Heroine_power < ApplicationRecord
+    belongs_to :heroines
+    belongs_to :powers
+end
