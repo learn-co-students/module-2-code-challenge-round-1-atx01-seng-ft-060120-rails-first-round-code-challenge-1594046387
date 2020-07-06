172fb9ed233acf93f9b475e50f02cfb71526d6cf
bradc1993 <bradc1993@gmail.com>
2020-07-06 13:25:03 -0500

    Done.

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,29 @@
 class PowersController < ApplicationController
+  before_action :get_power, only: [:show, :edit, :update]
+
   def index
     @powers = Power.all
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @power.update(power_params)
+    redirect_to power_path(@power)
+  end
+
+  private
+
+  def get_power
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description, :heroine_ids)
+  end
+
 end
---
+++ db/migrate/20200706144856_create_heroine_powers.rb
@@ -0,0 +1,8 @@
+class CreateHeroinePowers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :heroine_powers do |t|
+      t.belongs_to :heroine
+      t.belongs_to :power
+    end
+  end
+end
